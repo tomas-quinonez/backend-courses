83b4fdc030a4e4699b355b5b838599ea03b6d92c
Tomas <tomaslq98@gmail.com>
2024-04-28 22:13:32 -0300

    define docker compose

---
+++ sql/dbcreation.sql
@@ -0,0 +1,52 @@
+CREATE SCHEMA courses;
+
+
+CREATE TABLE courses.categories (
+	idcategory serial NOT NULL,
+	name varchar NOT NULL,
+	description varchar NOT NULL,
+	CONSTRAINT categories_pk PRIMARY KEY (idcategory)
+);
+
+CREATE TABLE courses.platforms (
+	idplatform serial NOT NULL,
+	name varchar NOT NULL,
+	description varchar NOT NULL,
+	CONSTRAINT platforms_pk PRIMARY KEY (idplatform)
+);
+
+CREATE TABLE courses.learningpaths (
+	idpath serial NOT NULL,
+	name varchar NOT NULL,
+	description varchar NOT NULL,
+	CONSTRAINT learningpaths_pk PRIMARY KEY (idpath)
+);
+
+CREATE TABLE courses.courses (
+	idcourse serial NOT NULL,
+	idcategory serial NOT NULL,
+	idplatform serial NOT NULL,
+	code int NOT NULL,
+	name varchar NOT NULL,
+	description varchar NOT NULL,
+	idpath int NULL,
+	CONSTRAINT courses_pk PRIMARY KEY (idcourse),
+	CONSTRAINT courses_categories_fk FOREIGN KEY (idcategory) REFERENCES courses.categories(idcategory),
+	CONSTRAINT courses_platforms_fk FOREIGN KEY (idplatform) REFERENCES courses.platforms(idplatform),
+	CONSTRAINT courses_learningpaths_fk FOREIGN KEY (idpath) REFERENCES courses.learningpaths(idpath)
+);
+
+
+
+INSERT INTO courses.categories
+(idcategory, name, description)
+VALUES(1, 'computacion', 'Categoria de computacion');
+
+INSERT INTO courses.platforms
+(idplatform, name, description)
+VALUES(1, 'udemy', 'Plataforma de aprendizaje en línea');
+
+INSERT INTO courses.courses
+(idcourse, idcategory, idplatform, code, name, description, idpath)
+VALUES(1, 1, 1, 5, 'Progrmación full-stack', 'Curso de programación web full-stack', NULL);
+
--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,7 @@
 const Sequelize = require('sequelize');
 
 module.exports = new Sequelize('courses_db', 'postgres', 'admin', {
-    host: 'localhost',
+    host: 'postgres',
+    port: '5432',
     dialect: 'postgres', // Change to your database type
 });
